b872dba975e65b8588dadaab46a7bc9cd583c1ee
Ruslan Isupov <111610909+Ruslan-Isupov@users.noreply.github.com>
2023-06-14 23:02:29 +0300

    ADD: mangoose to server.js

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
-const app = require('./app')
+const { error } = require("console");
+const app = require("./app");
+const mangoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST =
+  "mongodb+srv://Ruslan:Fc7MChunUN7yVRUl@cluster0.2ykrai3.mongodb.net/contacts_db?retryWrites=true&w=majority";
+
+mangoose
+  .connect(DB_HOST)
+  .then(() => app.listen(3000))
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
+mangoose.set("strictQuery", true);
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000");
+// });
