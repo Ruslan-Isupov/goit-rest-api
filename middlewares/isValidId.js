5ebc7719e0e3337e61662047c7116bfa4246c5df
Ruslan Isupov <111610909+Ruslan-Isupov@users.noreply.github.com>
2023-06-16 02:59:08 +0300

    DELETE:console.log in isValid.js

--- middlewares/isValidId.js
+++ middlewares/isValidId.js
@@ -3,9 +3,8 @@ const { HttpError } = require("../helpers");
 
 const isValidId = (req, res, next) => {
   const { contactId } = req.params;
-  //   console.log(req.params);
+
   if (!isValidObjectId(contactId)) {
-    // console.log(req.params);
     next(HttpError(400, `${contactId} is non valid id`));
   }
   next();
