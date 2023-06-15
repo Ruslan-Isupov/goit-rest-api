21ad792be84a18e87e9e1a112f9a32da113cbbb9
Ruslan Isupov <111610909+Ruslan-Isupov@users.noreply.github.com>
2023-06-15 14:51:35 +0300

    ADD:dotenv, env.example

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
-const { error } = require("console");
+// const { error } = require("console");
 const app = require("./app");
 const mangoose = require("mongoose");
 
-const DB_HOST =
-  "mongodb+srv://Ruslan:Fc7MChunUN7yVRUl@cluster0.2ykrai3.mongodb.net/contacts_db?retryWrites=true&w=majority";
-
+const { DB_HOST } = process.env;
 mangoose
   .connect(DB_HOST)
   .then(() => app.listen(3000))
@@ -12,9 +10,3 @@ mangoose
     console.log(error.message);
     process.exit(1);
   });
-
-mangoose.set("strictQuery", true);
-
-// app.listen(3000, () => {
-//   console.log("Server running. Use our API on port: 3000");
-// });
