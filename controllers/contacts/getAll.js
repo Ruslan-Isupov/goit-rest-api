969182f5d41a2b2d1ccb54a229be8c6e781c4869
Ruslan Isupov <111610909+Ruslan-Isupov@users.noreply.github.com>
2023-06-25 00:13:17 +0300

    ADD: pagination and filtration

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -1,14 +1,19 @@
 const { Contact } = require("../../models");
 
-// const { HttpError, ctrlWrapper } = require("../helpers");
-// const { model } = require("mongoose");
 const getAll = async (req, res) => {
   const { _id: owner } = req.user;
-  // const { page = 1, limit = 2, ...query } = req.query;
-  console.log(Contact);
-  // const skip = (page - 1) * limit;
-  // const data = await Contact.find({ owner, query }, { skip, limit }).populate("owmer", "name email")
-  const data = await Contact.find({ owner });
+  const { page = 1, limit = 20, ...query } = req.query;
+
+  const skip = (page - 1) * limit;
+  const data = await Contact.find(
+    { owner, ...query },
+    "-createdAt -updatedAt",
+    {
+      skip,
+      limit,
+    }
+  ).populate("owner", "email");
+
   console.log(data);
   res.json(data);
 };
--- schemas/contacts.js
+++ schemas/contacts.js
@@ -5,6 +5,7 @@ const contactsSchema = Joi.object({
   email: Joi.string().required(),
   phone: Joi.string().required(),
   favorite: Joi.boolean(),
+  // owner: Joi.string(),
 });
 
 const updateFavoriteSchema = Joi.object({ favorite: Joi.boolean().required() });
