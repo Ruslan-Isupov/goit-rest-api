3fa83d5da8e94a3ad52b1008d698cbced72c4ad9
Ruslan Isupov <111610909+Ruslan-Isupov@users.noreply.github.com>
2023-06-27 13:24:15 +0300

    ADD:folder public and tmt, miiddleware for static files

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const cors = require("cors");
 
 require("dotenv").config();
 
+// const multer = require("multer");
+// const path = require("path");
+// const fs = require("fs");
+
 const { authRouter } = require("./routes/api");
 const { contactsRouter } = require("./routes/api");
 
@@ -14,7 +18,7 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-
+app.use(express.json("public"));
 app.use("/api/contacts", contactsRouter);
 app.use("/api/auth", authRouter);
 
@@ -26,5 +30,27 @@ app.use((err, req, res, next) => {
   const { status = 500, message = "Server error" } = err;
   res.status(status).json(message);
 });
+
+// const tempDir = path.join(__dirname, "temp");
+// const multerConfig = multer.diskStorage({
+//   destination: tempDir,
+// });
+// const upload = multer({
+//   storage: multerConfig,
+// });
+
+// const contactsDir = path.join(__dirname, "public", "contacts");
+
+// app.post("/api/contacts", upload.single("avatar"), async (req, res) => {
+//   const { path: tempUploadd, originalname } = req.file;
+//   const resultUpload = path.join(contactsDir, originalname);
+//   await fs.rename(contactsDir, resultUpload);
+//   const avatar = path.join("contacts", originalname);
+//   const newConntacts = {
+//     id,
+//     ...req.body,
+//     avatar,
+//   };
+// });
 
 module.exports = app;
