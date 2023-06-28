52650e70a8f2c682f78e87f60c0fe11be5253c0e
Ruslan Isupov <111610909+Ruslan-Isupov@users.noreply.github.com>
2023-06-28 17:17:24 +0300

    DELETE: unnecessary code

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,7 +1,6 @@
 const multer = require("multer");
 const path = require("path");
 
-// const tempDir = path.join(__dirname, "../", "tmp");
 const tempDir = path.join(__dirname, "../", "tmp");
 
 const multerConfig = multer.diskStorage({
